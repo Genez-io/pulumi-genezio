1ade3362c91c74d91afa38312ccb624e628195a9
Andreia Ocănoaia <andreia.ocanoaia@gmail.com>
2024-07-29 15:47:27 +0300

    Fix linting warnings and remove logs
    Signed-off-by: Andreia Ocănoaia <andreia.ocanoaia@gmail.com>

--- provider/cloud_adapters/cloudAdapter.go
+++ provider/cloud_adapters/cloudAdapter.go
@@ -11,26 +11,36 @@ import (
 	p "github.com/pulumi/pulumi-go-provider"
 )
 
+<<<<<<< HEAD
 type CloudAdapter interface{
 	Deploy(ctx p.Context, input []domain.GenezioCloudInput, projectConfiguration domain.ProjectConfiguration, cloudAdapterOptions CloudAdapterOptions, stack *string) (domain.GenezioCloudOutput, error)
 	DeployFrontend(ctx p.Context,projectName string, projectRegion string, frontend domain.FrontendConfiguration,stage string) (string, error)
+=======
+type CloudAdapter interface {
+	Deploy(input []domain.GenezioCloudInput, projectConfiguration domain.ProjectConfiguration, cloudAdapterOptions CloudAdapterOptions, stack *string, authToken string) (domain.GenezioCloudOutput, error)
+	DeployFrontend(projectName string, projectRegion string, frontend domain.FrontendConfiguration, stage string, authToken string) (string, error)
+>>>>>>> cf969b7 (Fix linting warnings and remove logs)
 }
 
 type genezioCloudAdapter struct {
-
 }
 
 func NewGenezioCloudAdapter() CloudAdapter {
 	return &genezioCloudAdapter{}
 }
 
+<<<<<<< HEAD
 func (g *genezioCloudAdapter) Deploy(ctx p.Context, input []domain.GenezioCloudInput, projectConfiguration domain.ProjectConfiguration, cloudAdapterOptions CloudAdapterOptions, stack *string) (domain. GenezioCloudOutput, error) {
+=======
+func (g *genezioCloudAdapter) Deploy(input []domain.GenezioCloudInput, projectConfiguration domain.ProjectConfiguration, cloudAdapterOptions CloudAdapterOptions, stack *string, authToken string) (domain.GenezioCloudOutput, error) {
+>>>>>>> cf969b7 (Fix linting warnings and remove logs)
 
 	stage := ""
 	if cloudAdapterOptions.Stage != nil {
 		stage = *cloudAdapterOptions.Stage
 	}
-	
+<<<<<<< HEAD
+
 		for _, element := range input {
 			presignedUrl,err := requests.GetPresignedUrl(ctx, projectConfiguration.Region, "genezioDeploy.zip",projectConfiguration.Name, element.Name)
 			if err != nil {
@@ -46,20 +56,39 @@ func (g *genezioCloudAdapter) Deploy(ctx p.Context, input []domain.GenezioCloudI
 				return domain.GenezioCloudOutput{}, err
 			}
 
-			
-		} 
-	
-		response, err:= requests.DeployRequest(ctx,projectConfiguration, input, stage, nil) 
+
+		}
+
+		response, err:= requests.DeployRequest(ctx,projectConfiguration, input, stage, nil)
+=======
+
+	for _, element := range input {
+		presignedUrl, err := requests.GetPresignedUrl(projectConfiguration.Region, "genezioDeploy.zip", projectConfiguration.Name, element.Name, authToken)
+>>>>>>> cf969b7 (Fix linting warnings and remove logs)
 		if err != nil {
-			fmt.Printf("An error occurred while trying to deploy the request %v\n", err)
+			fmt.Printf("An error occurred while trying to get the presigned url %v\n", err)
 			return domain.GenezioCloudOutput{}, err
 		}
 
+		err = requests.UploadContentToS3(&presignedUrl, element.ArchivePath, nil)
+		if err != nil {
+			fmt.Printf("An error occurred while trying to upload the content to S3 %v\n", err)
+			return domain.GenezioCloudOutput{}, err
+		}
+
+	}
+
+	response, err := requests.DeployRequest(projectConfiguration, input, stage, nil, authToken)
+	if err != nil {
+		fmt.Printf("An error occurred while trying to deploy the request %v\n", err)
+		return domain.GenezioCloudOutput{}, err
+	}
+
 	return domain.GenezioCloudOutput{
-		ProjectID: response.ProjectID,
+		ProjectID:    response.ProjectID,
 		ProjectEnvID: response.ProjectEnvID,
-		Classes: response.Classes,
-		Functions: response.Functions,
+		Classes:      response.Classes,
+		Functions:    response.Functions,
 	}, nil
 }
 
@@ -70,11 +99,11 @@ func (g *genezioCloudAdapter) DeployFrontend(ctx p.Context, projectName string, 
 		finalStageName = fmt.Sprintf("-%s", stage)
 	} else {
 		finalStageName = ""
-	} 
+	}
 
 	finalSubdomain := fmt.Sprintf("%s%s", frontend.Subdomain, finalStageName)
 
-	temporaryFolder,err := utils.CreateTemporaryFolder(nil,nil)
+	temporaryFolder, err := utils.CreateTemporaryFolder(nil, nil)
 	if err != nil {
 		fmt.Printf("An error occurred while trying to create a temporary folder %v\n", err)
 		return "", err
@@ -84,10 +113,10 @@ func (g *genezioCloudAdapter) DeployFrontend(ctx p.Context, projectName string, 
 	if frontend.Publish == "" {
 		frontend.Publish = "."
 	}
-	frontendPath := filepath.Join(frontend.Path,frontend.Publish)
+	frontendPath := filepath.Join(frontend.Path, frontend.Publish)
 
-	exclussionList := []string{".git",".github"}
-	err = utils.ZipDirectoryToDestinationPath(frontendPath,finalSubdomain ,archivePath,exclussionList)
+	exclussionList := []string{".git", ".github"}
+	err = utils.ZipDirectoryToDestinationPath(frontendPath, finalSubdomain, archivePath, exclussionList)
 	if err != nil {
 		fmt.Printf("An error occurred while trying to zip the directory %v\n", err)
 		return "", err
@@ -99,7 +128,7 @@ func (g *genezioCloudAdapter) DeployFrontend(ctx p.Context, projectName string, 
 		return "", err
 	}
 
-	err = requests.UploadContentToS3(&presignedUrl.PresignedURL,archivePath,&presignedUrl.UserID)
+	err = requests.UploadContentToS3(&presignedUrl.PresignedURL, archivePath, &presignedUrl.UserID)
 	if err != nil {
 		fmt.Printf("An error occurred while trying to upload the content to S3 %v\n", err)
 		return "", err
@@ -110,13 +139,10 @@ func (g *genezioCloudAdapter) DeployFrontend(ctx p.Context, projectName string, 
 		fmt.Printf("An error occurred while trying to create the frontend project %v\n", err)
 		return "", err
 	}
-	
-	fmt.Printf("Frontend deployed successfully at %s\n", finalDomain)
+
 	return finalDomain, nil
 }
-
 
 type CloudAdapterOptions struct {
 	Stage *string `pulumi:"stage"`
 }
-
