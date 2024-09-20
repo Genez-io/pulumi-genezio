3a5c91b020d8dbdd720e05dbd6144f0f60d43dd8
Virgil993 <turcu.virgil@gmail.com>
2024-09-20 08:36:50 +0000

    fix examples

--- examples/nodejs/function/app.mjs
+++ examples/nodejs/function/app.mjs
@@ -1,5 +1,6 @@
 export const handler = async (event) => {
   const name = event.queryStringParameters?.name || "World";
+  console.log("The environment variable is:", process.env.ENV_VAR);
   return {
     statusCode: 200,
     body: `Hello ${name}!`,
